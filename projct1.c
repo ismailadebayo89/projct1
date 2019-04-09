80b9dddaaa8fe728c0d7a64e1c626f317419e73c
ismail Adebayo <ismail.adebayo89@gmail.com>
2019-04-09 01:26:25 +0000

    this only works for the rotation cipher

--- projct1.c
+++ projct1.c
@@ -1,37 +1,40 @@
-#include <stdio.h>
+##include <stdio.h>
 
 int main()
 {
-   int i, x;
-   char str[100];
-
-   printf("\nPlease enter statement:\t");
-   gets(str);
-
-   printf("\nPlease choose following options:\n");
-   printf("1 = Encrypt the statement.\n");
-   printf("2 = Decrypt the statement.\n");
-   scanf("%d", &x);
-
-   //using switch case statements
-   switch(x)
-   {
-   case 1:
-      for(i = 0; (i < 100 && str[i] != '\0'); i++)
-        str[i] = str[i] + 3; //the key for encryption is 3 that is added to ASCII value
-
-      printf("\nEncrypted string: %s\n", str);
-      break;
-
-   case 2:
-      for(i = 0; (i < 100 && str[i] != '\0'); i++)
-        str[i] = str[i] - 3; //the key for encryption is 3 that is subtracted to ASCII value
-
-      printf("\nDecrypted string: %s\n", str);
-      break;
-
-   default:
-      printf("\nError\n");
+ int i, x, y;
+ char str[100];
+    
+    printf("\nPlease enter a sentence:\t");
+    gets(str);
+	printf("\nPlease choose encryption method\n");
+	printf("1 = Rotation Encryption\n");
+	printf("2 = Substitution Encryption\n");
+	scanf("%d", &x);
+    if(1){
+        printf("\nPlease choose following options:\n");
+        printf("1 = Encrypt the string.\n");
+        printf("2 = Decrypt the string.\n");
+        scanf("%d", &y);
+       switch(x)
+       {
+       case 1:
+          for(i = 0; (i < 100 && str[i] != '\0'); i++)
+            str[i] = str[i] + 3; //the key for encryption is 3 that is added to ASCII value
+    
+          printf("\nEncrypted string: %s\n", str);
+          break;
+    
+       case 2:
+          for(i = 0; (i < 100 && str[i] != '\0'); i++)
+            str[i] = str[i] - 3; //the key for encryption is 3 that is subtracted to ASCII value
+    
+          printf("\nDecrypted string: %s\n", str);
+          break;
+    
+       default:
+          printf("\nError\n");
    }
+    }
    return 0;
 }
